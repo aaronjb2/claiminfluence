3e3dec8344bd25ff5f346390cecd82c992fa09b6
Aaron Braithwaite <aaronjbraithwaite@gmail.com>
2020-10-17 09:26:15 -0600

    removed localhost for deployment

--- src/app/web-socket.service.ts
+++ src/app/web-socket.service.ts
@@ -8,7 +8,8 @@ import { Observable } from 'rxjs';
 export class WebSocketService {
 
   socket: any;
-  readonly uri: string = "ws://localhost:4848";
+  // readonly uri: string = "ws://localhost:4848"; for running locally
+  readonly uri: string = ""; // for hosted
 
   constructor() { 
     this.socket = io(this.uri);
