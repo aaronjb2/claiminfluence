e51404194df82892569937e082e2f526b79c6298
Aaron Braithwaite <aaronjbraithwaite@gmail.com>
2020-11-01 11:58:53 -0700

    uncommented

--- server/server.js
+++ server/server.js
@@ -16,9 +16,9 @@ const io = socket(
     })
 );
 
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../build/index.html'))
-//   })
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build/index.html'))
+  })
 
 app.get('/getGameData/:identifier', (req, res) => {
     const { identifier } = req.params;
