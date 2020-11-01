e531307678af273ee59efffbaefb6d04e869e346
Aaron Braithwaite <aaronjbraithwaite@gmail.com>
2020-11-01 12:23:24 -0700

    removed the thing

--- server/server.js
+++ server/server.js
@@ -16,9 +16,9 @@ const io = socket(
     })
 );
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../build/dist/claiminfluence/src/index.html'))
- })
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../build/dist/claiminfluence/src/index.html'))
+//  })
 
 app.get('/getGameData/:identifier', (req, res) => {
     const { identifier } = req.params;
