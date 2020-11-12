3d82471afc493826404e1b4c86d683b56293c157
Aaron Braithwaite <aaronjbraithwaite@gmail.com>
2020-11-12 16:36:03 -0700

    tried another thing

--- server/server.js
+++ server/server.js
@@ -16,6 +16,11 @@ const io = socket(
     })
 );
 
+app.get('*', function (req, res) {
+    const index = path.join(__dirname, 'build', 'index.html');
+    res.sendFile(index);
+  });
+
 // app.get('*', (req, res) => {
 //     res.sendFile(path.join(__dirname, '../build/dist/claiminfluence/src/index.html'))
 //  })
